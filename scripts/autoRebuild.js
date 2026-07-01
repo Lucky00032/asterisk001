542fa8baba713c1c51b7e9ddf4110ef24dcc2e8f
Lucky00032 <amaguet78@gmail.com>
2026-06-30 23:45:55 -0400

    Update autoRebuild.js

--- scripts/autoRebuild.js
+++ scripts/autoRebuild.js
@@ -1,37 +1,25 @@
-const MAX_PLACEMENT_DISTANCE = 576;
-const PLACEMENT_PREVENTION_TICKS = 200;
+const processedTowers = new Set();
 
+const MAX_UPGRADE_DISTANCE = 768;
+const AUTO_UPGRADE_COOLDOWN_TICKS = 10;
 
-function autoRebuild() {
-    if (!this.myStash || !this.scripts.autoRebuild || this.missingAutoRebuildBuildings.size === 0) return;
+function autoUpgrade() {
+    if (!this.myStash || !this.scripts.autoUpgrade) return;
 
-    this.missingAutoRebuildBuildings.forEach(building => {
-        const x = building.x + this.myStash.x;
-        const y = building.y + this.myStash.y;
-
-        if (this.getDistance({ x, y }) > MAX_PLACEMENT_DISTANCE) return;
-        if (!this.enoughPartyMembers(building.type)) return;
-
-        // if anti pressure bug is on and the tower was just sold due to pressure bug then dont instantly rebuild;
-        if (this.scripts.antiPressureBug && this.justSold.has(`${x},${y}`)) return;
-        // if the tower was sold due to anti pressure bug and theres a player close-by then dont rebuild.
-        let canPlace = true;
-        if (this.scripts.antiPressureBug && this.wasSold.has(`${x},${y}`)) {
-            const width = ["Wall", "SlowTrap", "Door"].includes(building.type) ? 36 : 72;
-            // the widths are tweaked a bit to make it harder to pressure bug
-            const nearbyEntity = this.SpatialHash.queryClosest(this.options.sessionId, { x, y }, width);
-
-            if (this.enemies.has(nearbyEntity)) canPlace = false;
-        };
-        // if theres a player nearby then dont rebuild towers sold by APB.
-        if (!canPlace) {
-            this.justSold.add(`${x},${y}`);
-            this.waitTicks(PLACEMENT_PREVENTION_TICKS, () => this.justSold.delete(`${x},${y}`))
-            return;
-        };
-        // if theres no players nearby and it wasnt sold by APB then rebuild(rebuild checks if the cells are unoccuped)
-        this.makeBuilding(building.type, { x, y }, building.yaw);
-    });
+    this.missingAutoUpgradeBuildings.forEach(building => {
+        if (processedTowers.has(building.uid)) return;
+        // if the tower is supposed to be upgraded to the stash tier then dont delay upgrades
+        const originalBuilding = this.autoUpgradeBuildings.get(`${building.x - this.myStash.x},${building.y - this.myStash.y},${building.type}`);
+        if (this.getDistance(building) < MAX_UPGRADE_DISTANCE && (originalBuilding.tier === this.myStash.tier ? 20 : this.ticks + this.bumpUp) % 20 === 0) {
+            // add the building to a set so multiple alts dont autoUpgrade if the building tier is lower than stash tier
+            if (originalBuilding.tier !== this.myStash.tier) {
+                processedTowers.add(building.uid)
+                this.waitTicks(AUTO_UPGRADE_COOLDOWN_TICKS, () => processedTowers.delete(building.uid))
+            }
+            // upgrade the building
+            this.upgradeBuilding(building.uid)
+        }
+    })
 };
 
-export { autoRebuild };
+export { autoUpgrade };
